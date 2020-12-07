771957426040620dd2b8d0e3e08a63bc0f10b134
Dmytro Shavaryn <shavarynd@gmail.com>
2020-12-07 18:32:25 +0200

    Solution with static fields

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,21 @@ public class Main {
     // Create 3 Students
     Student std1 = new Student();
     Student std2 = new Student("Jack");
-    Student std3 = new Student("John", 9);
+    Student std3 = new Student("John");
 
     std1.setName("Sam");
 
     std1.setRating(2);
     std2.setRating(6);
+    std3.setRating(8);
 
     // Average rating
-    System.out.println(Students.getAverageRating());
+    System.out.println(Student.getAvgRating());
 
     // Change the rating
     std3.changeRating(7);
 
     // Display Average rating again
-    System.out.println(Students.getAverageRating());
+    System.out.println(Student.getAvgRating());
   }
 }
